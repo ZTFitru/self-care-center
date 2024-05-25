457bb5a46db3c24eae76a6f54bedf4d6bf5ba926
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-05-24 23:45:53 -0400

    User never sees repeated message

--- main.js
+++ main.js
@@ -18,6 +18,9 @@ var mantra = [
 
 /*---------------------------------------VARIABLES--------------------------------------*/ 
 var radioSelection;
+var counter = affirmation.length;
+var messagesArray = []
+var messages
 /*----------------------------------------VIEWS-----------------------------------------*/ 
 var messageView = document.querySelector('.budda')
 /*---------------------------------------BUTTON ----------------------------------------*/ 
@@ -46,14 +49,45 @@ function selfCareMantra(event) {
 
 function selfCareMessage(event) {
    if(radioSelection === 'affirmation') {
-    var messages = affirmation[Math.floor(Math.random() * affirmation.length)]
+    var number = Math.floor(Math.random() * affirmation.length)
+    if(affirmation.length > 0 ){
+        messages = affirmation[number]
+    } else if (affirmation.length <= 0 ) {
+        messages = "You will now see repeating messages."
+        affirmation = [
+            "I forgive myself and set myself free.", 
+            "I believe I can be all that I want to be.",
+            "I am in the process of becoming the best version of myself.",
+            "I have the freedom & power to create the life I desire.",
+            "I choose to be kind to myself and love myself unconditionally."
+            ]
+    }
     messageView.style.display = "none"
     randomMessage.innerHTML = messages
+    if(randomMessage.innerHTML != "You will now see repeating messages.") {
+        affirmation.splice(number, 1)
+    }
    }
-   else if(radioSelection === 'mantra') {
-    messages = mantra[Math.floor(Math.random() * mantra.length)]
-    messageView.style.display = 'none'
+    if(radioSelection === 'mantra') {
+    number = Math.floor(Math.random() * mantra.length)
+    if(mantra.length > 0) {
+        messages = mantra[number]
+    }
+    else if(mantra.length <= 0) {
+        messages = "You will now see repeating messages."
+        mantra = [
+            "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
+            "Don’t let yesterday take up too much of today.",
+            "Every day is a second chance.",
+            "Tell the truth and love everyone.",
+            "I am free from sadness."
+        ]
+    }
+    messageView.style.display = "none"
     randomMessage.innerHTML = messages
+    if(randomMessage.innerHTML != "You will now see repeating messages.") {
+        mantra.splice(number, 1)
+    }
    }
 
    event.preventDefault();
